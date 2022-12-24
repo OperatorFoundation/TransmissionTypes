c2b32863c4fce017167ed91e2d6e7ceb0c138002
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2022-12-24 13:33:32 -0600

    Added CodableConnection

---
+++ Sources/TransmissionTypes/CodableConnection.swift
@@ -0,0 +1,47 @@
+//
+//  CodableConnection.swift
+//  
+//
+//  Created by Dr. Brandon Wiley on 12/24/22.
+//
+
+import Foundation
+
+public class CodableConnection<ReadType, WriteType> where ReadType: Codable, WriteType: Codable
+{
+    let connection: Connection
+    let prefixLength: Int
+
+    public init(connection: Connection, prefixLength: Int)
+    {
+        self.connection = connection
+        self.prefixLength = prefixLength
+    }
+
+    public func read() throws -> ReadType
+    {
+        guard let data = self.connection.readWithLengthPrefix(prefixSizeInBits: self.prefixLength) else
+        {
+            throw CodableConnectionError.readError
+        }
+
+        let decoder = JSONDecoder()
+        return try decoder.decode(ReadType.self, from: data)
+    }
+
+    public func write(_ message: WriteType) throws
+    {
+        let encoder = JSONEncoder()
+        let data = try encoder.encode(message)
+        guard self.connection.write(data: data) else
+        {
+            throw CodableConnectionError.writeError
+        }
+    }
+}
+
+public enum CodableConnectionError: Error
+{
+    case readError
+    case writeError
+}
