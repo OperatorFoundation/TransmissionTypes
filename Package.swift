f025cb6bde3647b841ee3d0c9ea39533e533d6fd
consuelita <adelita@operatorfoundation.org>
2023-06-16 17:01:33 -0500

    support swift-tools-version:5.8 & macOS v13

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
     name: "TransmissionTypes",
-    platforms: [.macOS(.v10_15), .iOS(.v15)],
+    platforms: [.macOS(.v13), .iOS(.v15)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
