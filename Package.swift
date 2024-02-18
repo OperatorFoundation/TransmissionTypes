df5e18583662d5b77ea5d461480fd56b98ae8433
consuelita <adelita@operatorfoundation.org>
2024-02-18 11:22:21 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["TransmissionTypes"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/OperatorFoundation/Datable", from: "4.0.0"),
+        .package(url: "https://github.com/OperatorFoundation/Datable", from: "4.0.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
